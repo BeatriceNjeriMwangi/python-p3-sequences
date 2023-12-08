aaa14a15110dc6a3fa6db20bda90ccacf6081712
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2023-12-08 15:01:17 -0500

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    a,b = 0,1
+
+    for i in range(length):
+        fibonacci_sequence.append(i)
+        a,b =b, a+b
+    print(fibonacci_sequence)
+    
+print_fibonacci(9)
